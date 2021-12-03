8fbb83522d7ca13f596ab0677c37913389f5fa84
Maureen Njihia <maureennjihia468@gmail.com>
2021-12-03 17:30:53 +0300

    Register pages controller and add route function that uses pages controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController; //Register pages controller
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('pages.index');
 });
+*/
+
+Route::get('/', [PagesController::class, 'index']);
